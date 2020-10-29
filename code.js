b369abb75e2850642b1ab2e32e518113484bcaf7
BIT student <bitstudent@bitnet.wctc.edu>
2020-10-29 14:42:43 -0500

    added span tag output

--- code.js
+++ code.js
@@ -8,7 +8,8 @@ function calcLateFee() {
     //calc amt due
     var amtDue = numBooks * daysLate + numDVDs * daysLate;
     //output
-    alert("You owe: $"+amtDue.toFixed(2)+".");
+    //alert("You owe: $"+amtDue.toFixed(2)+".");
+	$("#libraryOut").text("You owe: $"+amtDue.toFixed(2)+".");
 }
 function calcPizzaShare() {
     //get num toppings
@@ -18,8 +19,8 @@ function calcPizzaShare() {
     //calc share amount
     var shareAmt = (15.0 + 1.25 * numToppings) / numCoworkers;
     //output
-    alert("Pizza cost is: $"+shareAmt.toFixed(2)+" per CoWorker.");
-
+    //alert("Pizza cost is: $"+shareAmt.toFixed(2)+" per CoWorker.");
+	$("#pizzaOut").text("Pizza cost is: $"+shareAmt.toFixed(2)+" per CoWorker.");
 }
 $(document).ready( function() {
 	$("#libraryBtn").click(calcLateFee);
